12d1c539e2d9de812c422c7675faf3f721186514
Yohana248 <152048862+Yohana248@users.noreply.github.com>
2026-09-20 11:45:25 +0300

    refactor: clear screen before show welcome screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "screens/WelcomeScreen.h"
+#include "utils/utils.h"
 
 int main()
 {
+  clearScreen();
   WelcomeScreen welcomeScreen;
   welcomeScreen.print();
+
+  return 0;
 }
